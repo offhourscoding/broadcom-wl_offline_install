0b177f947823e9cd0fa3a84bbd7501e41041bd39
Matt Adams <s0n1c2122@gmail.com>
2016-05-15 01:58:26 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,19 @@
+# Broadcom-wl Offline Install
+
+## SUMMARY
+Reinstalls the broadcom-wl Wireless driver on an Arch Linux machine.
+
+After upgrading the linux kernel on Arch, WiFi often stops working. This simple script reinstalls an offline copy of the broadcom-wl driver.
+
+This script does not include any dependencies, and should only be used to reinstall the driver.
+
+This script was tested on a Macbook Pro with the Broadcom BCM4331.
+
+
+## INSTALL
+
+    ./fix_wifi.bash
+
+
+## CHANGE LOG
+version 0.0.1 - Initial version (using broadcom-wl-6.30.223.271-2x86_64 driver)
---
+++ fix_wifi.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Simple script to reinstall the broadcom-wl Wifi driver
+#
+# After upgrading the linux kernel, Wifi stops working. This script
+# reinstalls the broadcom-wl driver from a local copy of the package.
+#
+# This script reinstalls the broadcom-wl driver only. It does not install
+# any of the other supported broadcom drivers
+#
+# Matt Adams (matt@offhourscoding.com)
+
+
+echo "---- Install broadcom driver ----"
+echo "================================="
+sudo pacman -U broadcom-wl-6.30.223.271-2-x86_64.pkg.tar.xz
+
+read -p "Would you like to reboot your computer now [y/n] ? " yn
+case $yn in
+  [Yy]* ) sudo reboot;;
+  [Nn]* ) echo "Okay. WiFi won't work until after reboot"
+esac
